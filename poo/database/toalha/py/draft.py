d0e9949038cfd95e99fe72db7648863af6f3f808
Bruna Viana <164355357+vianabrunn@users.noreply.github.com>
2025-09-16 20:23:46 +0000

    aprendendo a ativididade toalha 25minuto

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,1 +1,19 @@
-print("minha toalha é azul")
+
+class Towel:
+    def __init__(self, color: str, size: str): #construtor
+        self.color = color #atributos
+        self.size = size
+        self.wetness = 0
+
+    def __str__ (self):
+        return f"color:{self.color}, tam: {self.size}, umi: {self.wetness}"
+
+towel = Towel("green", "g") #refrencia e objetos
+towel2 = Towel("pink", "p")
+
+print(towel.color) #red
+towel.color = "white" 
+print(towel.color) #white
+print(towel.size)
+print(towel.wetness)
+print(towel)

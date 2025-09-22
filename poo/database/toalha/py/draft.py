6903c0cf1a02dfc0c90e6be71c3e6eae7ff5b1b1
Bruna Viana <164355357+vianabrunn@users.noreply.github.com>
2025-09-22 19:24:45 +0000

    add novas funções

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -5,15 +5,30 @@ class Towel:
         self.size = size
         self.wetness = 0
 
+    def dry(self, amount:int) -> None:
+        self.wetness += amount
+        if self.wetness >= self.getMaxWetness():
+            print("toalha encharcada")
+            self.wetness = self.getMaxWetness()
+    
+    def getMaxWetness(self) -> int:
+        if self.size == "p":
+            return 10
+        if self.size == "m":
+            return 20
+        if self.size == "g":
+            return 30
+        return 0
+
     def __str__ (self):
         return f"color:{self.color}, tam: {self.size}, umi: {self.wetness}"
 
-toalha = Towel("green", "g") #refrencia e objetos
-toalha = Towel("pink", "p")
 
-print(toalha.color) #red
-toalha.color = "white" 
-print(toalha.color) #white
-print(toalha.size)
-print(toalha.wetness)
-print(toalha)
+toalha_g = Towel("green", "g") #refrencia e objetos
+toalha_p = Towel("pink", "p")
+
+
+toalha_p.dry(5)
+print(toalha_p.wetness)
+toalha_p.dry(10)
+print(toalha_p.wetness)

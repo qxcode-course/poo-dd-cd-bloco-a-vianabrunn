0797f6fbccb671bb77d3e66d4265ea667e64cb70
bruna <bruna.vitoria@alu.ufc.br>
2025-09-30 22:08:49 -0300

    feito novamente com umas pesquinhas

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,1 +1,52 @@
+class Towel:
+    def __init__ (self, color: str, size: str):
+        self.cor = color
+        self.tamanho = size
+        self.wetness = 0
 
+    def dry (self, amount: int) -> None:
+         self.wetness += amount
+         if self.wetness >= self.getMaxWetness():
+            print("Toalha encharcada")
+            self.wetness = self.getMaxWetness()
+    
+    def wringOut (self) -> None :
+        self.wetness = 0
+
+    def getMaxWetness (self) -> int:
+        if self.tamanho == "p":
+            return 10
+        if self.tamanho == "m":
+            return 20
+        if self.tamanho == "g":
+            return 30
+        return 0
+
+    def isDry (self) -> bool:
+        return self.wetness == 0
+
+    def show(self) -> None:
+        print(self)
+    
+    def __str__ (self) -> str:
+        return f"color:{self.cor}, tam:{self.tamanho}, umi:{self.wetness}"
+
+def main():
+    toalha = Towel("", "")
+    while True:
+        line: str = input()
+        args: list[str] = line.split()
+        if args[0] == "end":
+            break        
+        elif args[0] == "new":
+            color = args[1]
+            size = args[2]
+            toalha = Towel(color, size)
+        elif args[0]  == "show":
+            print(toalha)
+        elif args[0] == "dry":
+            amount : int = int(args[1])
+            toalha.dry(amount)
+        else:
+            print("comando invalido")
+main()
